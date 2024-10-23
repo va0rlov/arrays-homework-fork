44ee8fc08bf09d641f867eae41b34d300e7adb6d
Виктор Орлов <vorlov@topsbi.ru>
2024-10-23 17:32:27 +0300

    'DefaultCustomArrayList' 1st version added

--- src/main/java/com/edu/DefaultCustomArrayList.java
+++ src/main/java/com/edu/DefaultCustomArrayList.java
@@ -1,51 +1,107 @@
 package com.edu;
 
+import java.util.Arrays;
 import java.util.Iterator;
 
 public class DefaultCustomArrayList<E> implements CustomArrayList<E> {
 
+    private static final int DEFAULT_CAPACITY = 10;
+    private E[] array;
+    private int size;
+
     public DefaultCustomArrayList() {
+        this.array = (E[]) new Object[DEFAULT_CAPACITY];
+        this.size = 0;
     }
 
     @Override
     public boolean add(E element) {
+        ensureCapacity();
+        array[size++] = element;
         return true;
+    }
+
+    private void ensureCapacity() {
+        if (size == array.length) {
+            int newCapacity = array.length * 2;
+            array = Arrays.copyOf(array, newCapacity);
+        }
     }
 
     @Override
     public boolean remove(E element) {
+        for (int i = 0; i < size; i++) {
+            if (array[i].equals(element)) {
+                remove(i);
+                return true;
+            }
+        }
         return false;
     }
 
     private void remove(int index) {
+        if (index < 0 || index >= size) {
+            throw new IndexOutOfBoundsException("Index: " + index + ", Size: " + size);
+        }
+        System.arraycopy(array, index + 1, array, index, size - index - 1);
+        array[--size] = null;
     }
 
     @Override
     public E get(int index) {
-        return null;
+        if (index < 0 || index >= size) {
+            throw new IndexOutOfBoundsException("Index: " + index + ", Size: " + size);
+        }
+        return array[index];
     }
 
     @Override
     public int size() {
-        return 0;
+        return size;
     }
 
     @Override
     public boolean isEmpty() {
-        return false;
+        return size == 0;
     }
 
     @Override
     public void clear() {
+        for (int i = 0; i < size; i++) {
+            array[i] = null;
+        }
+        size = 0;
     }
 
     @Override
     public boolean contains(E element) {
+        for (int i = 0; i < size; i++) {
+            if (array[i].equals(element)) {
+                return true;
+            }
+        }
         return false;
     }
 
     @Override
     public Iterator<E> iterator() {
-        return null;
+        return new Iterator<E>() {
+            private int currentIndex = 0;
+
+            @Override
+            public boolean hasNext() {
+                return currentIndex < size;
+            }
+
+            @Override
+            public E next() {
+                return array[currentIndex++];
+            }
+
+            @Override
+            public void remove() {
+                throw new UnsupportedOperationException("Remove operation is not supported");
+            }
+        };
     }
 }
